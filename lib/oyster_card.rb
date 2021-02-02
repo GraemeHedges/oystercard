510de92deb587705c66780dd1bb0f813afe7e0e8
Graemehedges <graeme.hedges@gmail.com>
2021-02-02 16:44:09 +0000

    added top up method, tested top up limit, tested error message

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,9 +1,15 @@
 class Oyster_card
 
+    TOP_UP_LIMIT = 90
+
     attr_reader :balance
 
     def initialize
         @balance = 0
     end
 
+    def top_up (value)
+        raise "You have exceeded the £#{TOP_UP_LIMIT} limit" if @balance + value > TOP_UP_LIMIT
+        @balance = balance + value
+    end
 end
