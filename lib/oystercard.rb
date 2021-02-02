a9195f4a9c66a96f2efdb4e92cbb497e268537ac
Graemehedges <graeme.hedges@gmail.com>
2021-02-02 19:49:53 +0000

    added touch_in, touch_out and in_journey? methods and tests (used predicate matcher tests)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,11 @@ class Oystercard
 
   TOP_UP_LIMIT = 90
 
-  attr_reader :balance
+  attr_reader :balance, :journey
 
   def initialize
     @balance = 0
+    @journey = false 
   end
 
   def top_up (value)
@@ -15,6 +16,18 @@ class Oystercard
 
   def deduct(value)
     @balance -= value
-  end 
+  end
+
+  def touch_in
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
+  end
+
+  def in_journey?
+    @journey
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,7 +15,7 @@ describe Oystercard do
 
     it "raises an error if the new balance would exceed the limit" do
       subject.top_up(Oystercard::TOP_UP_LIMIT)
-      expect{subject.top_up 1}.to raise_error "You have exceeded the £#{Oystercard::TOP_UP_LIMIT} limit"
+      expect{subject.top_up(1)}.to raise_error "You have exceeded the £#{Oystercard::TOP_UP_LIMIT} limit"
     end
   end
 
@@ -23,6 +23,36 @@ describe Oystercard do
     it "deducts the fare amount from the total balance" do
       subject.top_up (50)
       expect{subject.deduct 20}.to change {subject.balance}.by -20       
+    end
+  end
+
+  describe "#touch_in" do
+    it "touches the card in" do
+      expect(subject.touch_in).to eq true
+    end
+  end
+
+  describe "#touch_out" do
+    it "touches the card out" do
+      expect(subject.touch_out).to eq false
+    end
+  end
+
+  describe "#in_journey?" do
+
+    it "expect a new card not to be on a journey" do
+      expect(subject).not_to be_in_journey
+    end
+
+    it "when the card is touched in, expect it to be on a journey" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "when the card is touched out, at the end of a journey, expect it not be on a journey" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
     end
   end
 
